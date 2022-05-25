129f5b54bcd17bee157131250eb18f984059e6dd
ariotriguna05 <67588269+ariotriguna05@users.noreply.github.com>
2022-05-26 02:42:56 +0700

    Update getallpublic_ServiceGroup.js

--- object/servicegroup/getallpublic_ServiceGroup.js
+++ object/servicegroup/getallpublic_ServiceGroup.js
@@ -3,7 +3,7 @@ const env = require('dotenv').config();
 
 const request = supertest(process.env.productSevice_URL)
 
-const getallpublicServiceGroup = ()  => request.get('/public/v1/service-group')
+const getallpublicServiceGroup = ()  => request.get('/private/v1/service-group')
 .set('Content-Type', 'application/json')
 
 
